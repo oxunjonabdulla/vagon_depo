eabaf43f93d320f2602a2b98f559c289afe48fd0
HP <coderoxunjon50@gmail.com>
2024-06-18 18:02:50 +0500

    added db.sqlite .gitignore

--- root/settings.py
+++ root/settings.py
@@ -112,7 +112,9 @@ TIME_ZONE = 'Asia/Tashkent'
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
